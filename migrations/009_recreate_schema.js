1af82a6d4f8fa829c83cee9570d0d955b33a292d
Anderson Xavier <andersonxavier@pacctosolucoes.com.br>
2026-02-07 10:12:23 -0300

    iclusão da tabela mrkt ml

---
+++ migrations/009_recreate_schema.js
@@ -0,0 +1,13 @@
+/**
+ * Placeholder migration to satisfy migration history.
+ * This file was missing and caused the migration directory to be considered corrupt.
+ * It does not perform any schema changes.
+ */
+
+exports.up = async function(knex) {
+  // Intentionally empty placeholder
+};
+
+exports.down = async function(knex) {
+  // Intentionally empty placeholder
+};
